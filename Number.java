c645bf7598bba760b762406cc39e351fb0d2455f
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-21 23:53:16 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,19 @@
+class Number
+{
+	public static void main(String[] args){
+		int num = 454;
+		
+		if (num>0)
+		{
+			System.out.println("the number is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("the number is negative");
+		}
+		else 
+		{
+			System.out.println("the number is zero");
+		}
+	}
+}
