7183f662a4f7e1c413a3f464a04953c005c08700
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-22 23:19:58 +0530

    Add files via upload

---
+++ 21/07/3/Casting.java
@@ -0,0 +1,26 @@
+class Casting
+{
+public static void main(String[] args)
+{
+int i=81;
+char c;
+c =(char) i;
+
+System.out.println("The (int) value 81 is converted into (char)is: " +c);
+
+
+double d = 323.142; 
+i = (int)d;
+System.out.println("The (double) value 323.142 is converted into (int) is:" +i);
+
+
+float f =72.38f;
+i = (int) f;
+System.out.println("The (float) value is converted into (int) is: "+i);
+
+byte b; 
+b = (byte) d;
+System.out.println("The (double) value is coverted into (byte) is: " + b);
+
+}
+}
