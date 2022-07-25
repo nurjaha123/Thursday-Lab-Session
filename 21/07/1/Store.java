32487eceecb7a1952a01f3b9b1fccda7f42f3bef
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-25 15:57:06 +0530

    Add files via upload

---
+++ 21/07/1/Store.java
@@ -0,0 +1,26 @@
+class Store
+{
+ 
+public static void main(String[] args)
+{
+
+String customername = "Paul"; 
+String brand = "idol";
+double processor =2.66;
+byte ram =8;
+short hardDisk =500;
+byte monitor =15;
+char planA ='A';
+char planB = 'B';
+char planC = 'C';
+char planD = 'D';
+
+System.out.println("Customer Name: "+ customername + "(Date Type: String)");
+System.out.println("Brand Name: "+ brand + "(Data Type: String)");
+System.out.println("Processor speed: " + processor + "GHz" + "(Data Type: double)"); 
+System.out.println("Ram capacity: "+ ram + "GB" + "(Data Type: byte)"); 
+System.out.println("Hard Disk: " + hardDisk + "GB" +" (Data Type: short)"); 
+System.out.println("Monitor Size: " + monitor + "inch" +" (Data Type: byte)"); 
+System.out.println("Plans:" +planA +","+ planB+"," + planC+"," + planD + " (Data Type:char)"); 
+}
+}
