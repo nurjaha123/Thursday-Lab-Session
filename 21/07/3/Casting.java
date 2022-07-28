aae2def8cef12d69117e6395169c7c06a460f447
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-28 17:30:50 +0530

    Update Casting.java

--- 21/07/3/Casting.java
+++ 21/07/3/Casting.java
@@ -1,14 +1,3 @@
-3.When i is casted to c the value corresponding to the ascii code of 81 which is 'Q' is assigned.
-When d is casted to i the decimal part .142 is truncated and only 323 is assigned to i.
-Similarly when f is casted to i, the decimal part .38 is truncated and only 72 is assigned to i.
-When d is casted to b whose range is -128 to 127, not only the decimal part .142 is truncated but it
-is also shortened from 323 to 67 so as to fit in byte range.           
-i = 81 c = Q
-d = 323.142 i = 323
-f = 72.38 i = 72
-d = 323.142 b = 67
-
-
 class Casting
 {
 public static void main(String[] args)
