ea4ae178c39eb0300394ac69288cf10a0cd55fb2
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-28 17:31:47 +0530

    Update Store.java

--- 21/07/1/Store.java
+++ 21/07/1/Store.java
@@ -1,7 +1,3 @@
-1.Paul has bought a new notebook of "idol" brand, with a processor speed of 2.66GHz, 8 GB of RAM, 500GB hard disk, 
-with a 15-inch monitor, for $1760.55. He has chosen service plan 'C' among plans 'A', 'B', 'C', and 'D', 
-plus on-site servicing but did not choose extended warranty. Identify the data types and name the variables.
-
 
 class Store
 {
