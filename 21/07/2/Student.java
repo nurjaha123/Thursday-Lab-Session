9175cdaf3337cbfba1d9b7118cc06c358346b01d
nurjaha123 <109072782+nurjaha123@users.noreply.github.com>
2022-07-29 12:54:47 +0530

    Add files via upload

---
+++ 21/07/2/Student.java
@@ -0,0 +1,31 @@
+class Student
+{
+	public static void main(String []a)
+	{
+		String name = "geeta sharma";
+		String address = "pune, maharashtra";
+		long phone_no = 9075953765l;
+		char gender = 'F';
+		String dob = "26/02/1998";
+		float height = 1.56f; //m
+		float weight = 56.5f; //kg
+		String degree = "PhD";
+		short year = 2014;
+		float avg_gpa = 10.00f;
+		boolean ttn_grant = true;
+		boolean scholar = true;
+
+		System.out.println("Name - " +name);
+		System.out.println("Address - " +address);
+		System.out.println("Phone No. - " + phone_no);
+		System.out.println("Gender - "+ gender);
+		System.out.println("Date Of Birth - "+ dob);
+		System.out.println("Height - " + height + " m");
+		System.out.println("Weight - "+ weight + " kg");
+		System.out.println("Degree - "+ degree);
+		System.out.println("Study Year - " + year);
+		System.out.println("Average GPA - " + avg_gpa);
+		System.out.println("Tuition Grant - " +ttn_grant);
+		System.out.println("Scholar - " + scholar);
+	}	
+}
